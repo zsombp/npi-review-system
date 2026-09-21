a4e03041adf1dd9f6ce91a46686e3749723de8e4
Zsombor Petnehazi <zsomb.petnehazi@gmail.com>
2026-09-21 19:32:59 +0200

    web: deploy 2026-09-21T17:32Z from platform 0f5580c

--- assets/HelpLink-DrFw3spb.js
+++
@@ -1,1 +0,0 @@
-import{l as e,r as t,t as n}from"./jsx-runtime-BTXK9CdO.js";import{C as r}from"./index-H5D5x7Dc.js";var i=n();t(`hu`,{"help.link.label":`Súgó ehhez az oldalhoz`}),t(`en`,{"help.link.label":`Help for this page`});function a({section:t}){let{t:n}=e(),a=n(`help.link.label`);return(0,i.jsx)(r,{className:`help-link`,to:`/help#${t}`,title:a,"aria-label":a,children:`?`})}export{a as t};
